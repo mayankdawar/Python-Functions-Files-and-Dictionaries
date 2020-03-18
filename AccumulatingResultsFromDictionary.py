e3034658cab624920c48b10a2570a51898998d48
Mayank Dawar <mayankdawar29@gmail.com>
2020-03-19 00:53:43 +0530

    Create AccumulatingResultsFromDictionary.py

---
+++ AccumulatingResultsFromDictionary.py
@@ -0,0 +1,17 @@
+# he dictionary travel contains the number of countries within each continent that Jackie has traveled to. Find the total number of countries that Jackie has been to, and save this number to the variable name total. Do not hard code this!
+
+travel = {"North America": 2, "Europe": 8, "South America": 3, "Asia": 4, "Africa":1, "Antarctica": 0, "Australia": 1}
+total = 0
+for i in travel.keys():
+    total += travel[i] 
+    
+     
+    
+# schedule is a dictionary where a class name is a key and its value is how many credits it was worth. Go through and accumulate the total number of credits that have been earned so far and assign that to the variable total_credits. Do not hardcode.
+
+schedule = {"UARTS 150": 3, "SPANISH 103": 4, "ENGLISH 125": 4, "SI 110": 4, "ENS 356": 2, "WOMENSTD 240": 4, "SI 106": 4, "BIO 118": 3, "SPANISH 231": 4, "PSYCH 111": 4, "LING 111": 3, "SPANISH 232": 4, "STATS 250": 4, "SI 206": 4, "COGSCI 200": 4, "AMCULT 202": 4, "ANTHRO 101": 4}
+total_credits = 0
+for i in schedule:
+    total_credits += schedule[i]
+
+    
