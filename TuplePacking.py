a2d9b8916a7e5210690cf82e17c61375f9f6e4bf
Mayank Dawar <mayankdawar29@gmail.com>
2020-03-20 01:21:02 +0530

    Create TuplePacking.py

---
+++ TuplePacking.py
@@ -0,0 +1,13 @@
+# Provided is a list of tuples. Create another list called t_check that contains the third element of every tuple.
+
+
+lst_tups = [('Articuno', 'Moltres', 'Zaptos'), ('Beedrill', 'Metapod', 'Charizard', 'Venasaur', 'Squirtle'), ('Oddish', 'Poliwag', 'Diglett', 'Bellsprout'), ('Ponyta', "Farfetch'd", "Tauros", 'Dragonite'), ('Hoothoot', 'Chikorita', 'Lanturn', 'Flaaffy', 'Unown', 'Teddiursa', 'Phanpy'), ('Loudred', 'Volbeat', 'Wailord', 'Seviper', 'Sealeo')]
+t_check  = [ elmnt[2] for elmnt in lst_tups ]  
+
+
+
+# Below, we have provided a list of tuples. Write a for loop that saves the second element of each tuple into a list called seconds.
+
+tups = [('a', 'b', 'c'), (8, 7, 6, 5), ('blue', 'green', 'yellow', 'orange', 'red'), (5.6, 9.99, 2.5, 8.2), ('squirrel', 'chipmunk')]
+seconds = [ elmnt[1]  for elmnt in tups]
+
