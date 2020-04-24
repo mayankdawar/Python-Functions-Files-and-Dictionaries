c307ba64519ef1869375eb4b84d541972619d949
Mayank Dawar <mayankdawar29@gmail.com>
2020-04-24 21:00:45 +0530

    Update Project_part_1.py

--- Project_part_1.py
+++ Project_part_1.py
@@ -9,9 +9,6 @@ def strip_punctuation(Str):
     return Str
    
    
-   
-   
-   
 
 # Next, copy in your strip_punctuation function and define a function called get_pos which takes one parameter, a string which represents one or more sentences, and calculates how many words in the string are considered positive words. Use the list, positive_words to determine what words will count as positive. The function should return a positive integer - how many occurrences there are of positive words in the text. Note that all of the words in positive_words are lower cased, so you’ll need to convert all the words in the input string to lower case as well.
 
