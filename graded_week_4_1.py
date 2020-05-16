8f6e6c7b7e11c7c18504fa6065fd5014c003e448
Mayank Dawar <mayankdawar29@gmail.com>
2020-05-16 16:23:25 +0530

    Update graded_week_4_1.py

--- graded_week_4_1.py
+++ graded_week_4_1.py
@@ -60,6 +60,7 @@ while n < len(lst):
 
 
 
+
 # Challenge: Write a function called beginning that takes a list as input and contains a while loop that only stops once the element of the list is the string ‘bye’. What is returned is a list that contains up to the first 10 strings, regardless of where the loop stops. (i.e., if it stops on the 32nd element, the first 10 are returned. If “bye” is the 5th element, the first 4 are returned.) If you want to make this even more of a challenge, do this without slicing
 
 def beginning(lst):
@@ -69,6 +70,7 @@ def beginning(lst):
         newlst.append(lst[n])
         n += 1
     return newlst
+
 
             
             
